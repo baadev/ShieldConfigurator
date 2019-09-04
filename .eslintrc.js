f75c083110b3da4f3d1f6ef133ff54baace74d82
Alexander <ab2910811@gmail.com>
2019-09-04 13:29:31 +0300

    fixed lint indent (+fix)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,60 +1,46 @@
 // https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  env: {
-    browser: true,
-    node: true
-  },
-  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/recommended',
-    'prettier',
-    'prettier/vue'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {
-    // disallow reassignment of function parameters
-    // disallow parameter object manipulation except for specific exclusions
-    'no-param-reassign': 'off',
-    // TODO investigate a.b = !a.b assignments
-    // 'no-param-reassign': ['error', {
-    //   props: true,
-    //   ignorePropertyModificationsFor: [
-    //     'state', // for vuex state
-    //     'acc', // for reduce accumulators
-    //     'e' // for e.returnvalue
-    //   ]
-    // }],
-    // allow optionalDependencies
-    // allow debugger during development
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': ['error', 2, { 'VariableDeclarator': 'first' }],
-    'object-curly-spacing': ['error', 'always'],
-    'comma-dangle': ['error', 'never'],
-    'arrow-parens': ['error', 'as-needed'],
-    'max-len': ['error', { 'code': 120 }],
-    // Broken
-    'space-infix-ops': 'off',
-    // TODO enable later if really needed
-    'vue/require-v-for-key': 'off'
-  },
-  overrides: [
-    {
-      files: ['*.vue'],
-      rules: {
-        'indent': 2,
-        'vue/script-indent': [ '', 2, { 'baseIndent': 1 } ]
-      }
-    }
-  ]
+ root: true,
+ env: {
+  browser: true,
+  node: true
+ },
+ parserOptions: {
+  parser: "babel-eslint",
+  sourceType: "module"
+ },
+ // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+ extends: [
+  'eslint:recommended',
+  'plugin:vue/recommended',
+  'prettier',
+  'prettier/vue'
+ ],
+ // required to lint *.vue files
+ plugins: [
+  'vue'
+ ],
+ // add your custom rules here
+ rules: {
+  // 'no-param-reassign': 'off',
+  'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  'indent': ['error', 1, { 'VariableDeclarator': 'first', "MemberExpression": 1 }],
+  'object-curly-spacing': ['error', 'always'],
+  'comma-dangle': ['error', 'never'],
+  'arrow-parens': ['error', 'as-needed'],
+  'max-len': ['error', { 'code': 90 }]
+  // Broken
+  // 'space-infix-ops': 'off'
+  // 'vue/require-v-for-key': 'off'
+ },
+ overrides: [
+  {
+   files: ['*.vue'],
+   rules: {
+    'indent': ["off", 1, { "MemberExpression": 1 }],
+    'vue/script-indent': [ 'off', 2, { 'baseIndent': 1 } ]
+   }
+  }
+ ]
 }
